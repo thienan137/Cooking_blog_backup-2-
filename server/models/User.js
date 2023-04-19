6bb107f6c41a9dc6dcad17550e9c5267396caf35
thienan137 <nhta137@gmail.com>
2023-04-19 22:17:37 +0700

    fix bug

--- server/models/User.js
+++ server/models/User.js
@@ -18,25 +18,25 @@ const userSchema = new mongoose.Schema({
 });
 
 
-//fire a function before doc saved to db
-userSchema.pre('save', async function(next){
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
-});
-
-//staic method to login user
-userSchema.static.login = async function(email, password){
-    const user = await this.findOne({email});
-    if(user){
-        const auth = await bcrypt.compare(password, user.password);
-        if(auth){
-            return user;
-        }
-        throw Error('Incorrect password');
+  });
+  
+  // static method to login user
+  userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return user;
+      }
+      throw Error('incorrect password');
     }
-    throw Error('Incorrect email');
-}
+    throw Error('incorrect email');
+  };
 
 const User = mongoose.model('user', userSchema);
 
